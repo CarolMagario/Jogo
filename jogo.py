821b633c095bf05dd1764440837a4182604b30fc
Carol <carol.magario1@gmail.com>
2022-05-23 10:42:49 -0300

    inicio do jogo

--- jogo.py
+++ jogo.py
@@ -1,2 +1,19 @@
-import pygame as py
+import pygame 
+from pygame.locals import *
+
 pygame.init()
+ 
+screen = pygame.display.set_mode((576,1024))
+fps = pygame.time.Clock()
+
+#Declarar variáveis
+acabou = False
+
+#Loop principal do jogo
+while not acabou :
+    #processar eventos do mouse e do teclado
+    #lógica do jogo
+    #desenhar gráficos
+
+#fim do loop, fim do jogo
+pygame.quit()
