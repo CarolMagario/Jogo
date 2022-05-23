cc5d09c5407b662f71c0fe0574eddb7dbc3aa552
Carol <carol.magario1@gmail.com>
2022-05-23 17:49:09 -0300

    background e teste raposa

--- jogo.py
+++ jogo.py
@@ -1,12 +1,16 @@
+from turtle import window_height
 import pygame 
 from pygame.locals import *
 
 pygame.init()
  
-screen = pygame.display.set_mode((576,1024))
+screen = pygame.display.set_mode((600,800))
 fps = pygame.time.Clock()
 
 #Declarar variáveis
+BACKGROUND = pygame.image.load("Jogo/Imagens/33HF.gif").convert_alpha()
+
+raposa = pygame.image.load("Jogo/Imagens/fox2_preview_rev_1.png").convert_alpha()
 acabou = False
 pulo = False 
 
@@ -19,10 +23,18 @@ while not acabou :
                 acabou = True 
             if event.key == K_SPACE:
                 pulo = True
-        if event.type == MOUSEBUTTOMDOWN:
+        if event.type == MOUSEBUTTONDOWN:
             pulo = True
+
     #lógica do jogo
+
     #desenhar gráficos
+    screen.fill((0,0,0))
+    screen.blit(BACKGROUND, (0,0)) #background
+    screen.blit(raposa, (200,200))
+    #base
+    #canos
+    #bichinho
     pygame.display.update()
     fps.tick(25)
 
