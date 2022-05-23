c48eca4f0a3705b500108546ea340c6b41658480
Carol <carol.magario1@gmail.com>
2022-05-23 10:59:03 -0300

    variáveis I

--- jogo.py
+++ jogo.py
@@ -8,12 +8,23 @@ fps = pygame.time.Clock()
 
 #Declarar variáveis
 acabou = False
+pulo = False 
 
 #Loop principal do jogo
 while not acabou :
     #processar eventos do mouse e do teclado
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                acabou = True 
+            if event.key == K_SPACE:
+                pulo = True
+        if event.type == MOUSEBUTTOMDOWN:
+            pulo = True
     #lógica do jogo
     #desenhar gráficos
+    pygame.display.update()
+    fps.tick(25)
 
 #fim do loop, fim do jogo
 pygame.quit()
