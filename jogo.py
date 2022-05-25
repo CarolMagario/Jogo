55434cbe3fb3e51b159ddee0b4f4e5572b69d1ef
Carol <carol.magario1@gmail.com>
2022-05-25 14:23:06 -0300

    canos

--- jogo.py
+++ jogo.py
@@ -9,10 +9,21 @@ fps = pygame.time.Clock()
 
 #Declarar variáveis
 BACKGROUND = pygame.image.load("Jogo/Imagens/33HF.gif").convert_alpha()
-
+BASE = pygame.image.load("Jogo/Imagens/ground.png").convert_alpha()
 raposa = pygame.image.load("Jogo/Imagens/fox2_preview_rev_1.png").convert_alpha()
+#canos sem dimensão
+CANO_debaixo =  pygame.image.load("Jogo/Imagens/cano.png").convert_alpha()
+CANO_de_cima = pygame.transform.flip(CANO_debaixo, False, True).convert_alpha()
+#canos dimensionados
+tamanho_ideal = (240,460)
+CANO_top = pygame.transform.scale(CANO_de_cima,tamanho_ideal)
+CANO_bottom = pygame.transform.scale(CANO_debaixo,tamanho_ideal)
+cano_altura = 260
+cano_buraco = 300
+
 acabou = False
 pulo = False 
+cano = [200,100] # localização no eixo x dos dois canos, localização no eixo y do topo do cano
 
 #Loop principal do jogo
 while not acabou :
@@ -30,11 +41,19 @@ while not acabou :
 
     #desenhar gráficos
     screen.fill((0,0,0))
-    screen.blit(BACKGROUND, (0,0)) #background
-    screen.blit(raposa, (200,200))
+
+    #background
+    screen.blit(BACKGROUND, (0,0)) 
+    
     #base
+    screen.blit(BASE,(0,700))
+
     #canos
+    screen.blit(CANO_top, (cano[0], cano[1]-cano_altura))
+    screen.blit(CANO_bottom, (cano[0], cano[1]+cano_buraco))
+
     #bichinho
+    screen.blit(raposa, (200,200))
     pygame.display.update()
     fps.tick(25)
 
